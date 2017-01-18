9d7ac27ebfd5a61698277cf6b7268ce40230313d
mori <moritakazuki@moritakazutaka-no-MacBook-Air.local>
2017-01-18 16:56:38 +0900

    First commit

---
+++ app/src/main/java/jp/gr/java_conf/TaroIchinose/MainWidget.java
@@ -0,0 +1,10 @@
+package jp.gr.java_conf.TaroIchinose;
+
+import android.appwidget.AppWidgetProvider;
+
+/**
+ * Created by moritakazuki on 2017/01/18.
+ */
+
+public class MainWidget extends AppWidgetProvider {
+}
